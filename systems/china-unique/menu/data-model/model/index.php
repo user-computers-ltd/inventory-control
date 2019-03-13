13caf51227511502e45b6552a38a5e1e7f13a9cf
WaiChungWong <johnwongwwc@gmail.com>
2019-03-13 17:04:10 +0800

    updated model report - changed reserve to account for on hand only

--- systems/china-unique/menu/data-model/model/index.php
+++ systems/china-unique/menu/data-model/model/index.php
@@ -25,23 +25,25 @@
     $whereClause = $whereClause . "
       AND (
         IFNULL(c.qty_on_hand, 0) <> 0 OR
-        IFNULL(d.qty_on_order, 0) <> 0 OR
-        IFNULL(e.qty_on_reserve, 0) <> 0
+        IFNULL(d.qty_on_hand_reserve, 0) <> 0 OR
+        IFNULL(e.qty_on_order, 0) <> 0
       )
     ";
   }
 
   $results = query("
     SELECT
-      a.id                          AS `id`,
-      a.brand_code                  AS `brand_code`,
-      b.name                        AS `brand_name`,
-      a.model_no                    AS `model_no`,
-      a.cost_average                AS `cost_average`,
-      IFNULL(c.qty_on_hand, 0)      AS `qty_on_hand`,
-      IFNULL(d.qty_on_order, 0)     AS `qty_on_order`,
-      IFNULL(e.qty_on_reserve, 0)   AS `qty_on_reserve`,
-      IFNULL(f.qty_outstanding, 0)  AS `qty_outstanding`
+      a.id                              AS `id`,
+      a.brand_code                      AS `brand_code`,
+      b.name                            AS `brand_name`,
+      a.model_no                        AS `model_no`,
+      a.cost_average                    AS `cost_average`,
+      IFNULL(c.qty_on_hand, 0)          AS `qty_on_hand`,
+      IFNULL(d.qty_on_hand_reserve, 0)  AS `qty_on_hand_reserve`,
+      IFNULL(e.qty_on_order, 0)         AS `qty_on_order`,
+      IFNULL(f.qty_incoming, 0)         AS `qty_incoming`,
+      IFNULL(g.qty_incoming_reserve, 0) AS `qty_incoming_reserve`,
+      IFNULL(h.qty_on_demand, 0)        AS `qty_on_demand`
     FROM
       `model` AS a
     LEFT JOIN
@@ -57,6 +59,20 @@
     ON a.model_no=c.model_no AND a.brand_code=c.brand_code
     LEFT JOIN
       (SELECT
+        m.brand_code, m.model_no, SUM(m.qty) AS `qty_on_hand_reserve`
+      FROM
+        `sdo_model` AS m
+      LEFT JOIN
+        `sdo_header` AS h
+      ON m.do_no=h.do_no
+      WHERE
+        m.ia_no=\"\" AND
+        h.status=\"SAVED\"
+      GROUP BY
+        m.brand_code, m.model_no) AS d
+    ON a.brand_code=c.brand_code AND a.model_no=c.model_no
+    LEFT JOIN
+      (SELECT
         m.brand_code, m.model_no, SUM(GREATEST(qty_outstanding, 0)) AS `qty_on_order`
       FROM
         `po_model` AS m
@@ -66,24 +82,38 @@
       WHERE
         h.status='POSTED'
       GROUP BY
-        m.model_no, m.brand_code) AS d
-    ON a.model_no=d.model_no AND a.brand_code=d.brand_code
+        m.model_no, m.brand_code) AS e
+    ON a.model_no=e.model_no AND a.brand_code=e.brand_code
     LEFT JOIN
       (SELECT
-        m.brand_code, m.model_no, SUM(m.qty) AS `qty_on_reserve`
+        model_no, brand_code, SUM(qty) AS `qty_incoming`
+      FROM
+        `ia_model` AS m
+      LEFT JOIN
+        `ia_header` AS h
+      ON m.ia_no=h.ia_no
+      WHERE
+        h.status=\"DO\"
+      GROUP BY
+        m.brand_code, m.model_no) AS f
+    ON a.brand_code=f.brand_code AND a.model_no=f.model_no
+    LEFT JOIN
+      (SELECT
+        m.brand_code, m.model_no, SUM(m.qty) AS `qty_incoming_reserve`
       FROM
         `sdo_model` AS m
       LEFT JOIN
         `sdo_header` AS h
       ON m.do_no=h.do_no
       WHERE
+        m.ia_no<>\"\" AND
         h.status=\"SAVED\"
       GROUP BY
-        m.model_no, m.brand_code) AS e
-    ON a.brand_code=e.brand_code AND a.model_no=e.model_no
+        m.brand_code, m.model_no) AS g
+    ON a.brand_code=g.brand_code AND a.model_no=g.model_no
     LEFT JOIN
       (SELECT
-        m.brand_code, m.model_no, SUM(m.qty_outstanding) AS `qty_outstanding`
+        m.brand_code, m.model_no, SUM(m.qty_outstanding) AS `qty_on_demand`
       FROM
         `so_model` AS m
       LEFT JOIN
@@ -92,8 +122,8 @@
       WHERE
         h.status=\"CONFIRMED\"
       GROUP BY
-        m.brand_code, m.model_no) AS f
-    ON a.brand_code=f.brand_code AND a.model_no=f.model_no
+        m.brand_code, m.model_no) AS h
+    ON a.brand_code=h.brand_code AND a.model_no=h.model_no
     WHERE
       a.brand_code IS NOT NULL
       $whereClause
@@ -208,6 +238,7 @@
             <col style="width: 80px;">
             <col style="width: 80px;">
             <col style="width: 80px;">
+            <col style="width: 80px;">
           </colgroup>
           <thead>
             <tr></tr>
@@ -215,13 +246,14 @@
               <th rowspan="2">Brand</th>
               <th rowspan="2">Model No.</th>
               <th rowspan="2" class="number">Average Cost</th>
-              <th colspan="5" class="quantity">Quantity</th>
+              <th colspan="6" class="quantity">Quantity</th>
             </tr>
             <tr>
               <th class="number">On Hand</th>
-              <th class="number">On Reserve</th>
+              <th class="number">Reserved</th>
               <th class="number">Available</th>
               <th class="number">On Order</th>
+              <th class="number">On Demand</th>
               <th class="number">To Order</th>
             </tr>
           </thead>
@@ -231,6 +263,7 @@
               $totalOnReserve = 0;
               $totalAvailable = 0;
               $totalOnOrder = 0;
+              $totalOnDemand = 0;
               $totalToOrder = 0;
 
               for ($i = 0; $i < count($results); $i++) {
@@ -241,15 +274,17 @@
                 $modelNo = $model["model_no"];
                 $costAverage = $model["cost_average"];
                 $qtyOnHand = $model["qty_on_hand"];
-                $qtyOnReserve = $model["qty_on_reserve"];
+                $qtyOnReserve = $model["qty_on_hand_reserve"];
                 $qtyAvailable = max(0, $qtyOnHand - $qtyOnReserve);
                 $qtyOnOrder = $model["qty_on_order"];
-                $qtyToOrder = max(0, $model["qty_outstanding"] - $qtyOnHand - $qtyOnOrder);
+                $qtyOnDemand = $model["qty_on_demand"];
+                $qtyToOrder = max(0, $qtyOnDemand - $qtyOnHand - $qtyOnOrder);
 
                 $totalOnHand += $qtyOnHand;
                 $totalOnReserve += $qtyOnReserve;
                 $totalAvailable += $qtyAvailable;
                 $totalOnOrder += $qtyOnOrder;
+                $totalOnDemand += $qtyOnDemand;
                 $totalToOrder += $qtyToOrder;
 
                 echo "
@@ -261,7 +296,8 @@
                     <td class=\"number\" title=\"$qtyOnReserve\">" . number_format($qtyOnReserve) . "</td>
                     <td class=\"number\" title=\"$qtyAvailable\">" . number_format($qtyAvailable) . "</td>
                     <td class=\"number\" title=\"$qtyOnOrder\">" . number_format($qtyOnOrder) . "</td>
-                    <td class=\"number\" title=\"$qtyToOrder\">" . number_format($qtyToOrder) . "</td>
+                    <td class=\"number\" title=\"$qtyOnDemand\">" . number_format($qtyOnDemand) . "</td>
+                    <td class=\"number\" title=\"$qtyToOrder\">" . ($qtyToOrder > 0 ? number_format($qtyToOrder) : "-") . "</td>
                   </tr>
                 ";
               }
@@ -274,6 +310,7 @@
               <th class="number" title="<?php echo $totalOnReserve; ?>"><?php echo number_format($totalOnReserve); ?></th>
               <th class="number" title="<?php echo $totalAvailable; ?>"><?php echo number_format($totalAvailable); ?></th>
               <th class="number" title="<?php echo $totalOnOrder; ?>"><?php echo number_format($totalOnOrder); ?></th>
+              <th class="number" title="<?php echo $totalOnDemand; ?>"><?php echo number_format($totalOnDemand); ?></th>
               <th class="number" title="<?php echo $totalToOrder; ?>"><?php echo number_format($totalToOrder); ?></th>
             </tr>
           </tbody>
