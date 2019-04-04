54f5df71c2da7aca6ec3dbff0732cb2f61157170
WaiChungWong <johnwongwwc@gmail.com>
2019-04-04 10:32:41 +0100

    fixed failing disabled scroll on number input

--- includes/php/head.php
+++ includes/php/head.php
@@ -5,11 +5,21 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
 <link rel="stylesheet" href="<?php echo BASE_URL; ?>includes/css/main.css">
 <script>
-  window.onload = function() {
-    document.querySelector("body").addEventListener("scroll", function(event) {
-      if (event.target.matches("input[type=\"number\"]")) {
+  window.addEventListener("focus", function(event) {
+    var target = event.target;
+
+    if (target.matches && target.matches("input[type=\"number\"]")) {
+      var wheelHandler = function(event) {
         event.preventDefault();
-      }
-    }, true);
-  }
+      };
+
+      var blurHandler = function(event) {
+        event.target.removeEventListener("wheel", wheelHandler);
+        event.target.removeEventListener("blur", blurHandler);
+      };
+
+      target.addEventListener("wheel", wheelHandler);
+      target.addEventListener("blur", blurHandler);
+    }
+  }, true);
 </script>
